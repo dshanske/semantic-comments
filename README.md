ac8d6a3148ce3f16e44067b475f19034e719f386
David Shanske <david@shanske.com>
2014-05-24 23:37:18 -0400

    Version Bump

--- README.md
+++ README.md
@@ -3,4 +3,6 @@ semantic-comments
 
 Semantic Comments adds a Comment Template to Wordpress that Takes Advantage of Webmentions and the Semantic Linkbacks Plugin
 
+Version 0.02 - Adds Post Meta Boxes for Syndication URLs for the 4 major syndication sites(supported by Bridgy), add function to display these, add Twitter/Facebook/Google Plus/Instagram embed code functions, remove comment form in favor of webmention form and start of webactions section. 
+
 Version 0.01 - Replaces the Comments Template with one that has separate function calls for Webmentions, Comments, and Pingbacks/Tracksbacks. The Webmentions are displayed using a Facepile
--- semantic-comments.php
+++ semantic-comments.php
@@ -4,7 +4,7 @@
  * Plugin URI: http://david.shanske.com
  * Description: New Comment Templating that Offers Semantic Webmention Facepiles
  as well as separate Webmentions, Comments, and Trackbacks/Linkbacks. Styling is minimal
- * Version: 0.01
+ * Version: 0.02
  * Author: David Shanske
  * Author URI: http://david.shanske.com
  * License: CCO
@@ -12,6 +12,9 @@
 
 
 require_once( plugin_dir_path( __FILE__ ) . '/comment-handler.php');
+require_once( plugin_dir_path( __FILE__ ) . '/syndication.php');
+require_once( plugin_dir_path( __FILE__ ) . '/syn-postmeta.php');
+
 
 function semantic_comment_template( $comment_template ) {
         return plugin_dir_path( __FILE__ ) . '/comment-display.php';
@@ -21,6 +24,12 @@ add_filter( "comments_template", "semantic_comment_template" );
 
 function semantic_scripts() {
     wp_enqueue_style( 'comment-style', plugin_dir_url( __FILE__ ) . 'css/comment-style.css');
+     wp_enqueue_style( 'syndication-style', plugin_dir_url( __FILE__ ) . 'css/syndication-style.css');	
+ // Add Genericons font, for use in the main stylesheet.
+        wp_enqueue_style( 'genericons', plugin_dir_url( __FILE__ ) . '/generic
+ons/genericons.css', array(), '3.0.3' );
+
+
 }
 
 add_action( 'wp_enqueue_scripts', 'semantic_scripts' );
